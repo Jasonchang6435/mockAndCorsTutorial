0da18b948f4dabe7ab67faf987bd6c48519a23aa
jason chang <643501654@qq.com>
2018-04-02 10:11:11 +0800

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -17,10 +17,29 @@ CORS原理及解决方案
 
 
 mockServer 方案
-    node express 提供mock接口和数据
+    node express 提供mock接口和数据 可以用随意替换为同样功能的软件提供接口，如soapui
     nginx 提供转发请求和提供ui项目的server环境
     好处是
         这样你构建的好的前后端分离的项目可以在任意的目录调试
         也可以是你的git仓库目录 修改调试完可以直接push到远端仓库
+        
+nginx这里只提供nginx.conf文件 根据以下参考链接安装和自行配置
+       https://www.cnblogs.com/saysmy/p/6609796.html
 
+
+项目的使用：（这里是以windows为例）
+    express routes 
+        含mock名的为自行添加接口的文件 支持文件上传接口 
+    nginx 
+        windows环境下直接解压 linux/mac需要自行安装 
+        自定义conf/nginx.conf内的端口和ui文件夹绝对路径 
+        配置说明在nginx.conf带注释
+        
+    启动：
+        cd nginx根目录   start nginx
+        express node app.js
+        比如 express 配置端口是7000
+        nginx 默认端口9000 转发端口7000
+        前端项目是文件夹是/static
+        浏览器访问localhost：9000可以看到ui  http请求/api/* 都会请求到express上
 */
